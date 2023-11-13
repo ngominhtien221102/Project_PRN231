fae5322530c498337ab9bfcfe80eb6e5e9f21ccb
hungnx25 <hungnxhe160592@fpt.edu.vn>
2023-11-13 11:24:03 +0700

    update Project

--- MangaOnline/WebAPI/ResponseModels/DataResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Service.MangaOnline.ResponseModels;
-
-public class DataResponse
-{
-    public int status { get; set; }
-    public bool success { get; set; }
-    public string data { get; set; }
-}
--- MangaOnline/WebAPI/ResponseModels/HomeResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Service.MangaOnline.ResponseModels;
-
-public class HomeResponse
-{
-    public List<MangaResponse> TopMonthManga { get; set; }
-    public List<MangaResponse> NewUpdateMangas { get; set; }
-    public List<MangaResponse> NewDoneMangas { get; set; }
-    public List<MangaResponse> TopViewMangas { get; set; }
-}
